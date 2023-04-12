06c16edd7966653984854b1731ccb13111cb3a52
AlekseyOsipenkov <129942686+AlekseyOsipenkov@users.noreply.github.com>
2023-04-12 13:52:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 449719925 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,21 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+   
+significance = 0.07
+
+sample_success_a, sample_size_a = (x_success, x_cnt)
+sample_success_b, sample_size_b = (y_success, y_cnt)
+
+successes = np.array([sample_success_a, sample_success_b])
+samples = np.array([sample_size_a, sample_size_b])
+
+stat, p_value = proportions_ztest(count=successes, nobs=samples)
+
+print('z_stat: %0.3f, p_value: %0.10f' % (stat, p_value))
+
+ if p_value > significance:
+   return False
+else:
+  return True
+   
